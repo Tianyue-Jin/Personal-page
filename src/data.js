b47bf20cd023ae27c6d87e9135c1b16f32c2e12a
Tianyue-Jin <tyjin020726@gmail.com>
2023-03-02 16:48:01 +0000

    Personalized Summary

--- src/data.js
+++ src/data.js
@@ -2,13 +2,12 @@
 import {
   FaHtml5,
   FaCss3Alt,
-  FaBootstrap,
   FaReact,
   FaGitAlt,
   FaGithubSquare,
   FaLink,
 } from "react-icons/fa";
-import { SiJavascript, SiStyledcomponents, SiRedux } from "react-icons/si";
+import {SiJava,SiPython,SiCplusplus, SiJavascript} from "react-icons/si";
 
 // Projects Images (add your images to the images directory and import below)
 import Logo from "./images/logo.svg";
@@ -34,7 +33,7 @@ export const Blog = <FaLink />;
   Add a second paragraph for the about me section.
 */
 export const moreInfo =
-  "I enjoy learning about technology and helping others use it to improve their lives and be more productive. I built this site with React, React Bootstrap, Redux, and the GitHub REST API.";
+  "I am a second year Compuetr Science student studying in the University of Manchester. I am interested in machine learning and data analysis. I am looking forward to becoming a data scientist in the future";
 
 /* Skills
  ************************************************************** 
@@ -46,46 +45,55 @@ export const skillData = [
     skill: <FaHtml5 className="display-4" />,
     name: "HTML5",
   },
+
   {
     id: 2,
     skill: <FaCss3Alt className="display-4" />,
     name: "CSS3",
   },
+
   {
     id: 3,
     skill: <SiJavascript className="display-4" />,
     name: "JavaScript",
   },
+
   {
     id: 4,
-    skill: <FaBootstrap className="display-4" />,
-    name: "BootStrap",
+    skill: <SiJava className="display-4" />,
+    name: "Java",
   },
+
   {
     id: 5,
+    skill: <SiPython className="display-4" />,
+    name: "Python",
+  },
+
+  {
+    id: 6,
+    skill: <SiCplusplus className="display-4" />,
+    name: "C++",
+  },
+
+  {
+    id: 7,
     skill: <FaReact className="display-4" />,
     name: "React",
   },
-  {
-    id: 6,
-    skill: <SiStyledcomponents className="display-4" />,
-    name: "Styled Components",
-  },
-  {
-    id: 7,
-    skill: <SiRedux className="display-4" />,
-    name: "Redux",
-  },
+
   {
     id: 8,
     skill: <FaGitAlt className="display-4" />,
     name: "Git",
   },
+
   {
     id: 9,
     skill: <FaGithubSquare className="display-4" />,
     name: "GitHub",
-  },
+  }
+
 ];
 
 // Resume link (string - "https://YourResumeUrl") - I am using CloudFront to share my resume (https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/Introduction.html)
